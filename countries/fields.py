ed28597c6550c04074ae13ca68af319dea9a7367
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-02-28 17:59:21 +0100

    Refactored CountryField functionality out into a common Mixin and added a CountriesField which subclasses from ManyToMany field.

--- countries/fields.py
+++ countries/fields.py
@@ -1,18 +1,33 @@
-from django.db.models import ForeignKey
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class CountryField( ForeignKey ):
+class CountryFieldMixin(object):
     """
-    A ForeignKey to select a country.
-    
-    This field has a default value for verbose_name and a shortcut for 
+    Mixin class for `CountryField` and `CountriesField`.
+
+    This field has a default value for verbose_name and a shortcut for
     switching blank/null to True
     """
+
     def __init__( self, **kwargs ):
         kwargs.setdefault( 'verbose_name', _( 'countries' ) )
         if kwargs.pop( 'required', None ) == False:
             kwargs[ 'blank' ] = True
             kwargs[ 'null' ] = True
-        super( CountryField, self ).__init__( 'countries.Country', **kwargs )
+        super(CountryFieldMixin, self ).__init__( 'countries.Country',
+                                                 **kwargs)
+
+
+class CountryField(CountryFieldMixin, models.ForeignKey):
+    """
+    A ForeignKey to select a country.
+    """
+    pass
+
+
+class CountriesField(CountryFieldMixin, models.ManyToManyField):
+    """
+    A ManyToMany field to select multiple countries.
+    """
 
