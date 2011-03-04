6d58e4400985f8d8bd61a19a531080583b4fc3f7
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-03-04 11:20:12 +0100

    Fixed reference to README.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
     name='django-countries',
     version=__import__('countries').__version__,
     description='Provides various Django utilities for a complete list of world countries.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     # Get more strings from http://www.python.org/pypi?:action=list_classifiers
     author='Alex Kuhl',
     author_email='',
     url='http://github.com/alexkuhl/django-countries',
     download_url='',
     license='BSD',
-    packages=find_packages(exclude=['ez_setup']),
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False, # because we're including media that Django needs
     classifiers=[
