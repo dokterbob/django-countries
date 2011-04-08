c9c5622402e9154e151abb5f5173e8d4649d9368
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-04-08 16:23:50 +0200

    Attempt to translate language names.

--- countries/models.py
+++ countries/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext, ugettext_lazy as _
 
 class Country( models.Model ):
   """
@@ -35,5 +35,5 @@ class Country( models.Model ):
     
   def __unicode__( self ):
     ''' Return the display form of the country name'''
-    return self.name
-
+    english_name = unicode(self.name)
+    return ugettext(english_name)
