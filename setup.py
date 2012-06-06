3b7d13f0898908a416ef2cc7502add42302d7bdb
Kerin Cosford <cosford@gmail.com>
2012-06-06 16:52:14 +0200

    need to include xml files for initial_data fixtures to work

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     zip_safe=False, # because we're including media that Django needs
+    package_data = {'': ["*.xml"]},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
